22c8eccdcd4fc8ecfbae9272fc95b70cf58bd267
SmartStar <37624433+Dream-ark@users.noreply.github.com>
2019-11-17 20:00:59 +0800

    Update threeSum.java

--- threeSum.java
+++ threeSum.java
@@ -1,7 +1,9 @@
 【视频学习Day03_LeetCode15】
 //思路：1.暴力破解：依次遍历，寻找是否有-nums[i]==nums[j]+nums[k]，isInList和isSame用来去重。
+//2.双指针+排序：将数组排序；lo指针从前向后，hi指针从后向前,条件为三数和的大小是否等于0，如小于0，则lo++；反之hi--。
 
 class Solution {
+//法一：暴力破解
     public List<List<Integer>> threeSum(int[] nums) {
         int n=nums.length;
         List<List<Integer>> list=new ArrayList<>();
@@ -42,4 +44,34 @@ class Solution {
         }
         return false;
     }
+    
+    //法二：双指针+排序
+   public List<List<Integer>> threeSum(int[] nums) {
+      List <List <Integer>> list = new ArrayList <>();
+		Arrays.sort(nums);
+		for (int i = 0; i < nums.length() - 2; i++) {
+			if (nums[i] != nums[i - 1] || i == 0) {
+				int lo = i + 1, hi = nums.length - 1;
+				while(lo<hi){
+					if (nums[i] + nums[lo] + nums[hi] == 0) {
+						list.add(Arrays.asList(nums[i], nums[lo], nums[hi]));
+						while (nums[lo] == nums[lo + 1] && lo < hi) {
+							lo++;
+						}
+						while (nums[hi] == nums[hi + 1] && lo < hi) {
+							hi--;
+						}
+						lo++;
+						hi--;
+					} else if (nums[i] + nums[lo] + nums[hi] < 0) {
+						lo++;
+					} else {
+						hi--;
+					}
+				}
+				
+			}
+		}
+		return list;
+   }
 }
