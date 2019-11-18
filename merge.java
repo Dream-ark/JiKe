e627b1d9940c78ef16b1ddb9000222e234319926
SmartStar <37624433+Dream-ark@users.noreply.github.com>
2019-11-18 11:13:27 +0800

    Update merge.java

--- merge.java
+++ merge.java
@@ -9,18 +9,13 @@ class Solution {
         Arrays.sort(nums1);
     }
     
-    //法2：3指针法（时间复杂度低）O(1)
+    //法2：3指针法（时间复杂度低）O(1)【面试】
      public void merge(int[] nums1, int m, int[] nums2, int n) {
          int i=m-1;
 		int j=n-1;
 		int k = m+n-1;
 		while(i >=0 && j>=0)
-		{
-			if(nums1[i] > nums2[j])
-				nums1[k--] = nums1[i--];
-			else
-				nums1[k--] = nums2[j--];
-		}
+		nums1[k--]=nums1[i]>nums2[j]?nums1[i--]:nums2[j--];
 		while(j>=0)
 			nums1[k--] = nums2[j--];
     
