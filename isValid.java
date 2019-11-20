4a59527c3b900d7c837c61ad61a7bc1012deed34
SmartStar <37624433+Dream-ark@users.noreply.github.com>
2019-11-20 22:42:40 +0800

    Update isValid.java

--- isValid.java
+++ isValid.java
@@ -2,13 +2,50 @@
 //思路：1.暴力：依次遍历字符串元素：左括号存在list中；遇到右括号，先判断list中是否存有元素，有元素再判断在list中的最后一个元素是否和其匹配：
 //1.1匹配，将元素从list中删除；1.2不匹配，无效括号对。2.如果都是配对的，那么最后list为空。
 class Solution{
-  
-
-
-
-
-
-
-
-
+  public boolean isValid(String s){
+    List<Character> list =new ArrayList<>();
+    for(int i=0;i<s.length();i++){
+      if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='['){
+          list.add(s.charAt(i));
+      }
+      if(s.charAt(i)==')'||s.charAt(i)=='}'||s.charAt(i)==']'){
+          if(list.size()==0){
+              return false;
+          }
+          char ch=list.get(list.size()-1).charValue();
+          if(s.charAt(i)-ch!=1||s.charAt(i)-ch!=2){
+              return false;
+          }
+          list.remove(list.size()-1);
+      }
+    
+    }
+      if(list.size()==0){
+        return true;
+      }
+        return false;
+  }
+}
+//2.使用栈存储，复杂度O(n)
+class Solution{
+  public boolean isValid(String s){
+    Stack<Character> stack=new Stack<>();
+    if(s.length()==0){
+        return true;
+    }
+    for(int i=0;i<s.length();i++){
+      if(s.charAt(i)=='('){
+        stack.push(')');
+      }else if(s.charAt(i)=='{'){
+        stack.push('}');
+      }else if(s.charAt(i)=='['){
+        stack.push(']');
+      }else{
+        if(stack.isEmpty()||stack.pop()!=s.charAt(i)){
+          return false;
+        }
+      }
+    }
+    return stack.isEmpty();
+  }
 }
